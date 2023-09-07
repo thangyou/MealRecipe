298b3a4533b50ef1998e6d5fdec45140f9869dc8
YuneeeM <dana512@naver.com>
2023-09-07 19:47:40 +0900

    first commit

---
+++ src/main/java/doubleni/mealrecipe/MealRecipeApplication.java
@@ -0,0 +1,13 @@
+package doubleni.mealrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealRecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/doubleni/mealrecipe/MealRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package doubleni.mealrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
