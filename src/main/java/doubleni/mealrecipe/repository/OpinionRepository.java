dc4075fda7e769fb6a4fcb66f15d495d7b3b55c0
YuneeeM <dana512@naver.com>
2023-10-29 21:37:54 +0900

    Feat: #13 "DTO 삭제"

--- src/main/java/doubleni/mealrecipe/repository/OpinionRepository.java
+++
@@ -1,9 +0,0 @@
-package doubleni.mealrecipe.repository;
-
-import doubleni.mealrecipe.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OpinionRepository extends JpaRepository<User, Long> {
-}
